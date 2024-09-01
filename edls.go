c3d080637ab198b06aa40b76ef2d41302e7cf494
Jopsam <jopsam.07@gmail.com>
2024-09-01 11:58:03 -0400

    Add structs and flags.

--- edls.go
+++ edls.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 // Define file types constants with iota.
 const (
 	regularFile int = iota /* The regular file type is for files that are not defined by the others file type constants. */
@@ -22,3 +24,51 @@ const (
 	jpg    = ".jpg"
 	gif    = ".gif"
 )
+
+// Define file struct.
+type file struct {
+	name             string
+	fileType         int
+	isDirectory      bool
+	isHidden         bool
+	userName         string
+	groupName        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+// Define file type style struct.
+type fileTypeStyle struct {
+	icon   string
+	color  string
+	symbol string
+}
+
+var fileTypeStyleMap = map[int]fileTypeStyle{
+	regularFile: {
+		icon: "📄",
+	},
+	directoryFile: {
+		icon:   "📁",
+		color:  "blue",
+		symbol: "/",
+	},
+	executableFile: {
+		icon:   "🚀",
+		color:  "green",
+		symbol: "*",
+	},
+	compressedFile: {
+		icon:  "🗜️",
+		color: "red",
+	},
+	imageFile: {
+		icon:  "🖼️",
+		color: "magenta",
+	},
+	linkFile: {
+		icon:  "🔗",
+		color: "cyan",
+	},
+}
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	// Define a flag named p with a default value of an empty string. The String method returns a string pointer.
+	patternFlag := flag.String("p", "", "Filter by pattern.")
+	allFlag := flag.Bool("a", false, "List all files, including the hidden ones.")
+	numberRecordsFlag := flag.Int("n", 10, "Number of records to display.")
+	hasOrderByTimeFlag := flag.Bool("t", false, "Order by time, oldest first.")
+	hasOrderBySizeFlag := flag.Bool("s", false, "Order by size, smallest first.")
+	hasOrderReverseFlag := flag.Bool("r", false, "Reverse the order of the records.")
+
+	// Parse the command-line flags. The flag values are available after parsing.
+	flag.Parse()
+
+	// Print the flag value. We need to dereference the flag value to get the string value.
+	fmt.Println("patternFlag:", *patternFlag)
+	fmt.Println("allFlag:", *allFlag)
+	fmt.Println("numberRecordsFlag:", *numberRecordsFlag)
+	fmt.Println("hasOrderByTimeFlag:", *hasOrderByTimeFlag)
+	fmt.Println("hasOrderBySizeFlag:", *hasOrderBySizeFlag)
+	fmt.Println("hasOrderReverseFlag:", *hasOrderReverseFlag)
 }
